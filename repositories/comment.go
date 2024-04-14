6ebc8be3bdd859a73f2030bedb7bfee7336f3109
yutak03 <yutak03.dev@gmail.com>
2024-04-15 02:45:17 +0900

    Add get comment

--- repositories/comment.go
+++ repositories/comment.go
@@ -1,1 +1,49 @@
 package repositories
+
+import (
+	"database/sql"
+
+	"github.com/yutak03/go-http-api-server/models"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		INSERT INTO comments (article_id, message, created_at) VALUES
+		(?, ?, now());
+	`
+
+	var newComment models.Comment
+	newComment.ArticleID, newComment.Message = comment.ArticleID, comment.Message
+	
+	result, err := db.Exec(sqlStr, newComment.ArticleID, newComment.Message)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	id, _ := result.LastInsertId()
+
+	newComment.CommentID = int(id)
+	return newComment, nil
+}
+
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+		SELECT *
+		FROM comments
+		WHERE article_id = ?;
+	`
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+	for rows.Next() {
+		var comment models.Comment
+		rows.Scan(&comment.ArticleID, &comment.CommentID, &comment.Message, &comment.CreatedAt)
+
+		commentArray = append(commentArray, comment)
+	}
+	return commentArray, nil
+}
